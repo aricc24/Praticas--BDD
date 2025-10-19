a348487e23f556adcb3a7f07bd44709107bd3872
julietanilem <julietanilem@gmail.com>
2025-10-19 15:50:35 -0600

    prac5

---
+++ Practica05/script.sql
@@ -0,0 +1,42 @@
+
+----FUERTES
+
+CREATE TABLE Evento (
+    edicion INTEGER,
+    fecha DATE
+);
+
+ALTER TABLE Evento ADD PRIMARY KEY (edicion);
+
+-- Débiles
+
+CREATE TABLE CuentaPokemon (
+    id_persona INTEGER,
+    codigo_de_entrenador INTEGER,
+    equipo VARCHAR(20),
+    nivel INTEGER,
+    nombreDeUsuario VARCHAR(30),
+);
+
+ALTER TABLE CuentaPokemon ADD PRIMARY KEY (id_persona, codigo_de_entrenador);
+
+ALTER TABLE CuentaPokemon ADD FOREIGN KEY (id_persona) 
+    REFERENCES ParticipanteUNAM (id_persona);
+
+CREATE TABLE Pokemon (
+    id_pokemon INTEGER,
+    nombre VARCHAR(50),
+    sexo VARCHAR(10),
+    peso REAL,
+    puntosDeCombate INTEGER,
+    shiny BOOLEAN,
+    tipo VARCHAR(20),
+    especie VARCHAR(20),
+    id_persona INTEGER,
+    codigo_de_entrenador INTEGER
+);
+
+ALTER TABLE Pokemon ADD PRIMARY KEY (id_pokemon);
+ALTER TABLE Pokemon ADD FOREIGN KEY (id_persona, codigo_de_entrenador)
+    REFERENCES CuentaPokemon (id_persona, codigo_de_entrenador);
+
