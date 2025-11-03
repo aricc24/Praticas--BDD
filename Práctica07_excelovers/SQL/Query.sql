1674d6a1b88b9e8f40b3f6e3e6b95e27fefdaa3b
aricc24 <aricc24@ciencias.unam.mx>
2025-11-03 16:40:26 -0600

    query iii

--- Práctica07_excelovers/SQL/Query.sql
+++ Práctica07_excelovers/SQL/Query.sql
@@ -2,6 +2,7 @@
 SELECT * FROM participanteunam WHERE UPPER(nombre) LIKE 'R%';
 -- ii. Alimentos que hayan caducado después del 3 de noviembre del 2025 pero antes del 10 de diciembre
 -- del 2025.
+SELECT * FROM Alimento WHERE FechaDeCaducidad > '2025-11-03' AND FechaDeCaducidad < '2025-12-10'; 
 -- iii. Cuidadores que hayan nacido en el mes de noviembre.
 SELECT * FROM cuidador WHERE EXTRACT(MONTH FROM fechanacimiento) = 11;
 -- iv. Pokemones cuya especie sea Jigglypuff y cuyo CP sea 818.
