bd35433e65955a1826fe0d7fe9b56fc1248ffd09
LaraEnGithub <laronious@ciencias.unam.mx>
2025-11-11 15:13:59 -0600

    Documentacion

--- Práctica08_excelovers/SRC/Proyecto/exceloversdb/pokemon/admin.py
+++ Práctica08_excelovers/SRC/Proyecto/exceloversdb/pokemon/admin.py
@@ -1,6 +1,15 @@
+"""
+Configuración del panel de administración de Django para los modelos
+Vendedor y Alimento del proyecto Excelovers.
+"""
+
 from django.contrib import admin
 from .models import Vendedor, Alimento
 
+"""
+Clase que personaliza la forma en que se despliega la información
+de los vendedores en el administrador de Django.
+"""
 @admin.register(Vendedor)
 class VendedorAdmin(admin.ModelAdmin):
     list_display = (
@@ -19,6 +28,10 @@ class VendedorAdmin(admin.ModelAdmin):
         'ubicacion',
     )
 
+"""
+Clase que personaliza la forma en que se despliega la información
+de los alimentos en el administrador de Django.
+"""
 @admin.register(Alimento)
 class AlimentoAdmin(admin.ModelAdmin):
     list_display = (
