c3472bfd9626c7d0112e5861de1d23fd95a78db3
LaraEnGithub <laronious@ciencias.unam.mx>
2025-11-11 15:44:23 -0600

    Documentacion

--- Práctica08_excelovers/SRC/Proyecto/exceloversdb/pokemon/urls.py
+++ Práctica08_excelovers/SRC/Proyecto/exceloversdb/pokemon/urls.py
@@ -1,3 +1,8 @@
+"""
+Este módulo define los endpoints disponibles para interactuar con los
+recursos `Vendedor` y `Alimento` del proyecto Excelovers.
+"""
+
 from django.urls import path
 from .views import (
     VendedorApiView,
@@ -6,9 +11,12 @@ from .views import (
     AlimentoApiId,
 )
 
+#: Lista de rutas disponibles en la API.
 urlpatterns = [
+    # --- Rutas para Vendedor ---
     path('vendedor/', VendedorApiView.as_view()),
     path('vendedor/<int:idpersona>/', VendedorApiId.as_view()),
+    # --- Rutas para Alimento ---
     path('alimento/', AlimentoApiView.as_view()),
     path('alimento/<int:idalimento>/', AlimentoApiId.as_view()),
 ]
