60e05a7ceefa4b6aa924b7da0bedf891535cfd0b
LaraEnGithub <laronious@ciencias.unam.mx>
2025-11-11 15:19:07 -0600

    Documentacion

--- Práctica08_excelovers/SRC/Proyecto/exceloversdb/pokemon/apps.py
+++ Práctica08_excelovers/SRC/Proyecto/exceloversdb/pokemon/apps.py
@@ -1,6 +1,16 @@
+"""
+Módulo que define la clase de configuración de la aplicación, la cual
+permite registrar la app en el sistema de Django y ajustar parámetros
+como el campo automático por defecto para los modelos.
+"""
+
 from django.apps import AppConfig
 
-
+"""
+Esta clase define la configuración básica necesaria para que
+Django reconozca y gestione la aplicación. Hereda de
+`django.apps.AppConfig`.
+"""
 class PokemonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pokemon'
