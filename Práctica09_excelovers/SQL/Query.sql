d5fe1590f1decf73b726f9d74a4b701e69e3f520
LaraEnGithub <laronious@ciencias.unam.mx>
2025-11-17 20:07:15 -0600

    Querys V y VI

--- Práctica09_excelovers/SQL/Query.sql
+++ Práctica09_excelovers/SQL/Query.sql
@@ -1,20 +1,42 @@
 -- i. Mostrar el nombre completo de todos los participantes junto con su cuenta de Pokémon Go.
-SELECT  p.Nombre || ' ' || p.ApellidoPaterno || ' ' || p.ApellidoMaterno AS NombreCompleto, c.NombreDeUsuario AS CuentaPokemonGo
-FROM ParticipanteUNAM p JOIN CuentaPokemonGo c ON p.IdPersona = c.IdPersona;
-
 -- ii. Calcular cuántos Pokémons registró cada participante para el torneo de peleas por cada una de las ediciones.
-SELECT r.Edicion, r.IdPersona, COUNT(r.IdPokemon) AS TotalPokemonRegistrados FROM Registrar r
-GROUP BY r.Edicion, r.IdPersona 
-ORDER BY r.Edicion, r.IdPersona;
-
 -- iii. Listar todos los Pokémones cuya especie contenga la cadena ćhu ́
 -- .
 
 -- iv. Obtener la lista de participantes que estén inscritos en el Torneo de Captura de Shiny y a su vez que no estén
 -- inscritos en el torneo de distancia recorrida.
 -- v. Calcular la distancia total recorrida por cada participante en el torneo de distancia recorrida.
+SELECT
+    p.nombre,
+    p.apellidopaterno,
+    p.apellidomaterno,
+    dr.idparticipante,
+    SUM(dr.distancia) AS distancia_total
+FROM
+    DistanciaRecorrida dr JOIN ParticipanteUNAM p ON dr.idparticipante = p.idparticipante
+    JOIN TorneoDistanciaRecorrida t ON dr.idTorneoDistancia = t.idTorneoDistancia
+GROUP BY
+    p.nombre,
+    p.apellidopaterno,
+    p.apellidomaterno
+    dr.idparticipante
+ORDER BY
+    distancia_total
 -- vi. Listar los Pokémones shinys, que fueron capturados durante el evento, únicamente si fueron capturados entre
 -- las 14:00hrs y las 18:00hrs.
+SELECT
+    p.nombre AS nombre,
+    p.tipo,
+    p.shiny,
+    c.fechacaptura,
+    c.horacaptura,
+    e.edicion AS evento
+FROM
+    CapturaPokemon c JOIN Pokemon p ON c.idpokemon = p.idpokemon 
+    JOIN Evento e ON c.idevento = e.idevento
+WHERE
+    p.shiny = TRUE AND c.idevento IS NOT NULL 
+    AND c.horacaptura BETWEEN '14:00:00' AND '18:00:00'
 -- vii. Mostrar a todos los vendedores junto con los alimentos que venden, indicando el precio sin IVA y el precio final
 -- con IVA del 16 %.
 -- viii. Mostrar las facultades que tienen más de 5 participantes inscritos en cualquier torneo.
