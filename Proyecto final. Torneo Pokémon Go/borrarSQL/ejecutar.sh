89ea44ff967b5aa035724cc0313e1d632d7738fd
julietanilem <julietanilem@gmail.com>
2025-12-02 18:49:58 -0600

    tablas separadas

--- Proyecto final. Torneo Pokémon Go/borrarSQL/ejecutar.sh
+++ Proyecto final. Torneo Pokémon Go/borrarSQL/ejecutar.sh
@@ -2,6 +2,16 @@ docker exec -i postgres psql -U postgres -d prueba < Eventos.sql
 docker exec -i postgres psql -U postgres -d prueba < ParticipanteUNAM.sql
 docker exec -i postgres psql -U postgres -d prueba < CorreoParticipante.sql
 docker exec -i postgres psql -U postgres -d prueba < TelefonoParticipante.sql
+docker exec -i postgres psql -U postgres -d prueba < Limpiador.sql
+docker exec -i postgres psql -U postgres -d prueba < CorreoLimpiador.sql
+docker exec -i postgres psql -U postgres -d prueba < TelefonoLimpiador.sql
+docker exec -i postgres psql -U postgres -d prueba < Cuidador.sql
+docker exec -i postgres psql -U postgres -d prueba < CorreoCuidador.sql
+docker exec -i postgres psql -U postgres -d prueba < TelefonoCuidador.sql
+docker exec -i postgres psql -U postgres -d prueba < Encargado.sql
+docker exec -i postgres psql -U postgres -d prueba < CorreoEncargado.sql
+docker exec -i postgres psql -U postgres -d prueba < TelefonoEncargado.sql
+
 docker exec -i postgres psql -U postgres -d prueba < CuentaPokemon.sql
 docker exec -i postgres psql -U postgres -d prueba < Pokemon.sql
 
